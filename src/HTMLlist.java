6533fae813265eb223a19889d19a567d3e178110
n3urohazard <belea.simion@gmail.com>
2013-10-31 18:48:34 +0100

    Revert "Task 2 completed"
    This reverts commit ef5b967b5216e71b7a157946a6882e3750c8b9b1.

--- src/HTMLlist.java
+++ src/HTMLlist.java
@@ -1,44 +1,9 @@
-/**
- * Created with IntelliJ IDEA.
- * User: Julian
- * Date: 10/14/13
- * Time: 3:23 PM
- * To change this template use File | Settings | File Templates.
- */
+class HTMLlist {
+    String str;
+    HTMLlist next;
 
-public class HTMLlist {
-   String str;
-   HTMLlist next;
-   URLlist urls;
-
-   HTMLlist(String s, HTMLlist n){
-      str = s;
-      next = n;
-   }
-
-   HTMLlist(String s, HTMLlist n, URLlist u) {
-      str = s;
-      next = n;
-      urls = u;
-   }
-
-
-  /* class HTMLlistIterator {
-      private HTMLlist current;
-
-      HTMLlistIterator() {
-         current = HTMLlist.this.next;
-      }
-
-      public  boolean hasNext(){
-         return current != null;
-      }
-
-      public HTMLlist next(){
-         HTMLlist item = current;// get the next item
-         current = current.next; // move forwards
-         return item;
-      }*/
-
-
-  }
+    HTMLlist (String s, HTMLlist n) {
+        str = s;
+        next = n;
+    }
+}
